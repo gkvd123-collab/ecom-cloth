ee4da3e31c2187fe56ba13fe8e3cd6e4e240eb74
unknown <govind.kumar@talentpace.com>
2020-05-25 20:57:25 +0530

    Updates

---
+++ precache-manifest.e81c9c526dcd0cb749fac3146ddccce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c7675e959667add4024817ace11ed3",
+    "url": "/ecom-cloth/index.html"
+  },
+  {
+    "revision": "62360005967609b36b15",
+    "url": "/ecom-cloth/static/css/main.5a497f19.chunk.css"
+  },
+  {
+    "revision": "4485e61b6edc51243f46",
+    "url": "/ecom-cloth/static/js/2.9f91992c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ecom-cloth/static/js/2.9f91992c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62360005967609b36b15",
+    "url": "/ecom-cloth/static/js/main.354718fb.chunk.js"
+  },
+  {
+    "revision": "77bd13d2195a0d29db2c",
+    "url": "/ecom-cloth/static/js/runtime-main.e7eed197.js"
+  }
+]);
